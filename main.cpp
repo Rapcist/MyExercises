1a757e28435c6ef818557c5cd0c5cbdbeb732df7
Rapcist <bakulinsasha2004@yandex.ru>
2023-09-26 15:38:13 +0300

    -

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 #include <string>
+#include <format>
+#include <string_view>
 using namespace std;
 
+
+string func1(string_view  hays, string_view  needle, string_view  repl);
+
 int main() {
-	constexpr string abc{"1488"};
-	cout<<abc;
+	string haystack,needle,replstr;
+
+	getline(cin,haystack);
+	getline(cin,needle);
+	getline(cin,replstr);
+
+	cout<<format("\nInit: {} {} \n", haystack,& haystack);
+
+	haystack = func1(haystack,needle,replstr);
+
+	cout<<format("\nRes: {} {} \n", haystack,& haystack);
 	return 0;
+}
+
+string func1(string_view  hays, string_view  needle, string_view  repl){
+	string tmp;
+	for(size_t i {0}; i < hays.length();++i) {
+
+		if(hays[i] == needle[0]) {
+			size_t j {1}; 
+			for(; j < needle.length() && hays[i+j]==needle[j]; ++j);
+			++j;
+			if(j == needle.length()) { // replacement
+
+			}
+		}
+	}
 }

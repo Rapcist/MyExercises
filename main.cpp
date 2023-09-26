7b04a52e85f829236d70063ce38e9666f98662ff
Rapcist <bakulinsasha2004@yandex.ru>
2023-09-26 17:37:21 +0300

    ~done

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include <string>
 #include <format>
 #include <string_view>
+#include <algorithm>
 using namespace std;
 
 
-string func1(string_view  hays, string_view  needle, string_view  repl);
+string func1(string const &  hays, string_view  needle, string_view  repl);
+
 
 int main() {
 	string haystack,needle,replstr;
@@ -14,25 +16,28 @@ int main() {
 	getline(cin,needle);
 	getline(cin,replstr);
 
-	cout<<format("\nInit: {} {} \n", haystack,& haystack);
+	cout<<format("\nInit: {} \n", haystack);
 
 	haystack = func1(haystack,needle,replstr);
 
-	cout<<format("\nRes: {} {} \n", haystack,& haystack);
+	cout<<format("\nRes: {} \n", haystack);
 	return 0;
 }
 
-string func1(string_view  hays, string_view  needle, string_view  repl){
+string func1(string const& hays, string_view  needle, string_view  repl){
 	string tmp;
 	for(size_t i {0}; i < hays.length();++i) {
-
 		if(hays[i] == needle[0]) {
 			size_t j {1}; 
-			for(; j < needle.length() && hays[i+j]==needle[j]; ++j);
-			++j;
+			for(; j < needle.length() && i+j<hays.length() && hays[i+j]==needle[j]; ++j);
 			if(j == needle.length()) { // replacement
-
+				tmp += repl.data();
+				i += j ;
+				continue;
 			}
 		}
+		tmp.push_back(hays[i]);
 	}
+	return tmp;
 }
+

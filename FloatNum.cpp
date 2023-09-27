b704b3c6aeb84f145109d1da328c226a3597d750
Rapcist <138100279+Rapcist@users.noreply.github.com>
2023-09-27 09:52:30 +0300

    Check later

--- FloatNum.cpp
+++ FloatNum.cpp
@@ -4,5 +4,17 @@
 using namespace std;
 
 int main() {
-
+  vector<double> arr;
+  //конечно можно и без дополнительной перем.,
+  //но она у меня в массив попадает:((
+  double tmp{1};
+  while(cin>>tmp){
+    arr.push_back(tmp);
+  }
+  cout<<"Res:\n";
+  for(auto db : arr){
+    auto s {format("\n|{0:<8.3e}|{0:#>+12.5f}|{0:?^16.7g}|\n",db)};
+    cout<<s;
+    for(auto ch: s) cout<<"=";
+  }
 }
